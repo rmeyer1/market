288908ac57b0271a5aff38b701d84f4a07e963e9
Rob Meyer <robmeyer03@gmail.com>
2019-06-11 22:54:55 -0400

    Initial Commit

---
+++ src/server/database.js
@@ -0,0 +1,15 @@
+import Rebase from 're-base';
+import firebase from "firebase";
+
+
+const fireBaseApp  = firebase.initializeApp({
+    apiKey: "AIzaSyB1PH77QLNpPF7pcipfWkTv4hRO33JrzKM",
+    authDomain: "store-1-e5e86.firebaseapp.com",
+    databaseURL: "https://store-1-e5e86.firebaseio.com",
+});
+
+const base = Rebase.createClass(fireBaseApp.database());
+
+export { fireBaseApp };
+
+export default base;
